adf52af044c66bf70ae5d3dbfb5c43f76d6c73e7
ligy2016 <liguangyao010@gmail.com>
2016-05-12 16:43:08 +0800

    add dict

--- bs.py
+++ bs.py
@@ -9,17 +9,46 @@ html = """
 <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
 <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
 and they lived at the bottom of a well.</p>
-<p class="story">...</p>"""
+<p class="story">...</p>
+<a href="http://example.net/elsie" class="sister" id="link1"><!--  Elsie  --></a>"""
+
 # soup = BeautifulSoup(open('www.163.com'))
 soup = BeautifulSoup(html,"lxml")
 # print soup.prettify()
 # print soup.p.attrs
 #
 # print soup.a.string
-
+# print soup.find(attrs={href=True,algin=None})
+# print soup.find(True)
 # test github
 
 # for string in soup.stripped_strings :
 #     print(repr(string))
 # print soup.find_all('a')
-print soup.find_all(href=re.compile("elsie"))
+urldict = {}
+def addUrl(urldict, url, title):
+    urldict.setdefault(url, []).append(title)
+a_set = set()
+url_set = set()
+a_set = soup.find_all(href=re.compile("com"))
+for a in a_set:
+    # print a.string.strip(),a["href"]
+    url_set.add(a["href"])
+# soup.find_all(attrs={"id" = True, "algin" = None})
+new_a_set= set()
+new_url_set = set()
+# print type(new_url_set)
+new_a_set = soup.find_all(href=re.compile("."))
+
+for a in new_a_set:
+    # print a.string.strip(),a["href"]
+    new_url_set.add(a["href"])
+    addUrl (urldict,a["href"],a.string.strip())
+
+new_urls = new_url_set.difference(url_set)
+
+for new_url in new_urls:
+    print new_url, urldict[new_url]
+
+
+
