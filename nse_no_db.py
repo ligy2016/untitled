2f3e7c23b38d7be8c6d5a4859da3efbf86f7868d
ligy2016 <liguangyao010@gmail.com>
2016-05-16 16:46:44 +0800

    oo

--- nse_no_db.py
+++ nse_no_db.py
@@ -13,6 +13,38 @@ import copy
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+class scrawl_site():
+    def __init__(self,url):
+        self.url = url
+        self.urldict = {}
+        self.a_set = set()
+        self.url_set = set()
+        self.new_a_set = set()
+        self.new_url_set = set()
+        self.soup = BeautifulSoup()
+
+    def GetSiteContent(self):
+        req = urllib2.Request("http://money.163.com/")
+        req.add_header('Cache-Control', 'max-age=0')
+        resp = urllib2.urlopen(req)
+        self.soup = BeautifulSoup(resp.read(), "lxml")
+
+    def FindPattern(self):
+        self.new_a_set = self.soup.find_all(href=re.compile("163.com"), text=True)
+    def SaveContent(self):
+        for a in self.new_a_set:
+            self.new_url_set.add(a["href"])
+            self.addUrl(urldict, a["href"], a.string)
+
+        new_urls = self.new_url_set.difference(url_set)
+        print len(self.new_url_set), len(self.url_set), len(new_urls)
+        for new_url in new_urls:
+            print new_url, self.urldict[new_url][0]
+            # urldict.get(new_url, 'not found')
+
+        self.url_set = copy.deepcopy(self.new_url_set)
+        self.new_url_set.clear()
+        self.urldict.clear()
 
 def addUrl(urldict, url, title):
     urldict.setdefault(url, []).append(title)
@@ -28,14 +60,14 @@ def main():
     while(1):
         print "time.ctime() : %s" % time.ctime()
 
-        req = urllib2.Request("http://wallstreetcn.com/")
+        req = urllib2.Request("http://money.163.com/")
         req.add_header('Cache-Control', 'max-age=0')
         resp = urllib2.urlopen(req)
         # content = resp.read()
 
         soup = BeautifulSoup(resp.read(), "lxml")
 
-        new_a_set = soup.find_all(class_=re.compile("title"),href=re.compile("node"),text=True)
+        new_a_set = soup.find_all(href=re.compile("163.com"),text=True)
         for a in new_a_set:
 
             new_url_set.add(a["href"])
@@ -46,7 +78,6 @@ def main():
         for new_url in new_urls:
             print new_url, urldict[new_url][0]
             # urldict.get(new_url, 'not found')
-
 
         url_set = copy.deepcopy(new_url_set)
         new_url_set.clear()
