a585cfc08e1123d89bda1bc0c3aab783184b3271
ligy2016 <liguangyao010@gmail.com>
2016-09-27 17:55:17 +0800

    oo

---
+++ numpy_1.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+# import numpy as np
+from pandas import  *
+
+def test_pandas():
+    obj = series([4,7,-5,3])
+    print obj
+
+# arry1 = np.array(data1)
+# print arry1,arry1.dtype
+
+if __name__ == '__main__':
+    test_pandas()
