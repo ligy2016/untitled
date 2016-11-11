9aee5dbf8d920ac5b71b1c31ec1b67bec10e21c0
ligy2016 <liguangyao010@gmail.com>
2016-11-11 17:14:40 +0800

    oo

--- nse_kw.py
+++ nse_kw.py
@@ -11,6 +11,17 @@ class kws():
         self.domains = domains
         self.kw = kw
 
+    #解析当前页面
+    def parse_page(self):
+        req = urllib2.Request(self.url)
+        req.add_header('Cache-Control', 'max-age=0')
+        try:
+            resp = urllib2.urlopen(req, timeout=10)
+        except :
+            # return 1 #忽略超时等其他错误
+            pass
+        self.soup = BeautifulSoup(resp.read(), "lxml")
+        resp.close()
     #判断新的连接是否在指定域内
     def isindomain(self,link):
         for domain in self.domains:
@@ -19,7 +30,16 @@ class kws():
             if domain in link:
                 return True
         return False
-    #
+    #找到当前页面的所有的链接
+    def find_all_links(self):
+        for s in self.domains
+            self.new_a_set += self.soup.find_all(href=re.compile(s), text=True)
+            # self.new_a_set = self.soup.find_all(href=re.compile(".com"), text=True)
+        return
+    def link_is_searched(self):
+        if link in oldlinks
+            return True
+
     def keyword():
         pass
 
