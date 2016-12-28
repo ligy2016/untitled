272d66331b6d305f74624e82b07b034a48b57470
ligy2016 <liguangyao010@gmail.com>
2016-12-28 10:18:22 +0800

    刷新多张表

--- nse_no_db.py
+++ nse_no_db.py
@@ -30,6 +30,7 @@ class ScrawlSite():
             resp = urllib2.urlopen(req, timeout=10)
             self.soup = BeautifulSoup(resp.read(), "lxml")
             resp.close()
+
         except StandardError, e:
             print 'except', e
             return 1 #忽略超时等其他错误
@@ -38,16 +39,16 @@ class ScrawlSite():
         self.new_a_set = self.soup.find_all(href=re.compile(".com"), text=True)
 
     def GetNewLinks(self):
+        self._UpdateSet()
         for a in self.new_a_set:
             self.new_url_set.add(a["href"])
             self.urldict.setdefault(a["href"], []).append(a.string)
         self.new_urls = self.new_url_set.difference(self.pre_url_set)
-        print "45",self.new_urls
-        print len(self.new_url_set), len(self.pre_url_set), len(self.new_urls)
+        for url in self.new_urls: #打印显示每次新发现的链接
+            print ctime(),self.urldict.get(url, 'not found')[0].strip(),url
+        # print len(self.new_url_set), len(self.pre_url_set), len(self.new_urls)
 
-    def UpdateSet(self):
-        print "54",self.pre_url_set
-        print "55",self.new_url_set
+    def _UpdateSet(self):
 
         if(len(self.new_url_set) >0):
             self.pre_url_set = copy.deepcopy(self.new_url_set)
@@ -61,8 +62,7 @@ class ScrawlSite():
             return (None,None,None)
         self.FindPattern()
         self.GetNewLinks()
-
-        self.UpdateSet()
+        # self.UpdateSet()
 
 class SS_163(ScrawlSite):
     def FindPattern(self):
@@ -80,29 +80,77 @@ class DFCF_DHGD(ScrawlSite):#东方财富大行观点
         div = self.soup.find(id='newsListContent')
         self.new_a_set = div.find_all(href=True, text=True)
 
+
+class DFCF_USD(ScrawlSite):  # 东方财富美元
+    def FindPattern(self):
+        div = self.soup.find(id='newsListContent')
+        self.new_a_set = div.find_all(href=True)
+
+    def GetNewLinks(self):
+        self._UpdateSet()
+        for a in self.new_a_set:
+            self.new_url_set.add(a["href"])
+            title = re.sub(r'''<[^>]+>''', '', str(a))
+            self.urldict.setdefault(a["href"], []).append(title)
+        self.new_urls = self.new_url_set.difference(self.pre_url_set)
+        for url in self.new_urls:  # 打印显示每次新发现的链接
+            print ctime(), self.urldict.get(url, 'not found')[0].strip(), url
+class DFCF_WHPL(DFCF_DHGD):
+    pass
+class DFCF_JPY(DFCF_USD):
+    pass
+class DFCF_EUR(DFCF_USD):
+    pass
+class DFCF_GBP(DFCF_USD):
+    pass
+class DFCF_AUD(DFCF_USD):
+    pass
+class DFCF_NZD(DFCF_USD):
+    pass
+
 def main():
     db = mysql.MYSQL(host = "127.0.0.1", user="root", pwd="123456", db="new_schema")
-    # ss = SS_163(url = "http://money.163.com/",tablename = "urls")
-    # ss_wallstreet = SS_Wallstreet(url = "http://wallstreetcn.com/",tablename = "urls")
-    # ss_fx168 = SS_fx168(url = "http://www.fx168.com/forex/all/",tablename = "urls")
-
-    dfcf_dhgd = DFCF_DHGD(url = "http://forex.eastmoney.com/news/cdhgd.html",tablename = "dfcf_dhgd")
     db.ConnectDB()
 
+    dfcf_dhgd = DFCF_DHGD(url = "http://forex.eastmoney.com/news/cdhgd.html",tablename = "dfcf_dhgd")
+    dfcf_whpl = DFCF_WHPL(url="http://forex.eastmoney.com/news/cwhpl.html", tablename="dfcf_whpl")
+    dfcf_usd = DFCF_USD(url="http://forex.eastmoney.com/news/aUSD.html", tablename="dfcf_usd")
+    dfcf_jpy = DFCF_JPY(url="http://forex.eastmoney.com/news/aJPY.html", tablename="dfcf_jpy")
+    dfcf_eur = DFCF_EUR(url="http://forex.eastmoney.com/news/aEUR.html", tablename="dfcf_eur")
+    dfcf_gbp = DFCF_GBP(url="http://forex.eastmoney.com/news/aGBP.html", tablename="dfcf_gbp")
+    dfcf_aud = DFCF_AUD(url="http://forex.eastmoney.com/news/aAUD.html", tablename="dfcf_aud")
+    dfcf_nzd = DFCF_NZD(url="http://forex.eastmoney.com/news/aNZD.html", tablename="dfcf_nzd")
+
     dfcf_dhgd.pre_url_set = copy.deepcopy(set(db.SelectTopNUrls(tablename="dfcf_dhgd", n=20)))
-    print dfcf_dhgd.pre_url_set
+
     while(1):
-        # (tablename, urls, dict) = ss.gg()
-        # mysql.SaveUrls( 'urls', urls, dict)
-        # (tablename, urls, dict) = ss_wallstreet.gg()
-        # mysql.SaveUrls( 'urls', urls, dict)
-        # (tablename, urls, dict) = ss_fx168.gg()
-        # mysql.SaveUrls( 'urls', urls, dict)
         dfcf_dhgd.gg()
-        print (dfcf_dhgd.tablename, dfcf_dhgd.new_urls ,dfcf_dhgd.urldict)
-        # db.SaveUrls(tablename, urls, dict)
+        db.SaveUrls(dfcf_dhgd.tablename, dfcf_dhgd.new_urls, dfcf_dhgd.urldict)
+
+        dfcf_whpl.gg()
+        db.SaveUrls(dfcf_whpl.tablename, dfcf_whpl.new_urls, dfcf_whpl.urldict)
+
+        dfcf_usd.gg()
+        db.SaveUrls(dfcf_usd.tablename, dfcf_usd.new_urls ,dfcf_usd.urldict)
+
+        dfcf_jpy.gg()
+        db.SaveUrls(dfcf_jpy.tablename, dfcf_jpy.new_urls, dfcf_jpy.urldict)
+
+        dfcf_eur.gg()
+        db.SaveUrls(dfcf_eur.tablename, dfcf_eur.new_urls, dfcf_eur.urldict)
+
+        dfcf_gbp.gg()
+        db.SaveUrls(dfcf_gbp.tablename, dfcf_gbp.new_urls, dfcf_gbp.urldict)
+
+        dfcf_aud.gg()
+        db.SaveUrls(dfcf_aud.tablename, dfcf_aud.new_urls, dfcf_aud.urldict)
+
+        dfcf_nzd.gg()
+        db.SaveUrls(dfcf_nzd.tablename, dfcf_nzd.new_urls, dfcf_nzd.urldict)
+
+
         db.conn.commit()
-        break
+        # break
         sleep(300)
 
     db.DisconnectDB()
